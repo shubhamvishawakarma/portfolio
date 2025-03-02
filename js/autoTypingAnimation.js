33561daf158e8796f0167706e8cd80b4dbfbc600
shubhamvishawakarma <shubhamvishawakrma35@gmail.com>
2025-03-02 15:50:21 +0530

    shubham-portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Shubham Vishwakarma</span> a driven leader with a passion for self-improvement and embracing challenges as opportunities for growth. Continuously seeking to expand knowledge and skills through innovative and forward-thinking approaches. Dedicated to driving positive change through fresh perspectives and adaptability."
+      ],
+      typeSpeed: 1,
+      contentType: 'html'
+    });
+  }, 10);
